b47c89b571b0072e74ddfbe1b3df5437f04d7317
jjongsma <jeremy@jongsma.org>
2013-07-30 08:50:59 -0500

    Misunderstood auto_vacuum option

--- downpour/core/sqlitefk.py
+++ downpour/core/sqlitefk.py
@@ -25,8 +25,6 @@ class SQLiteFK(SQLite):
 
         # enable foreign keys
         raw_connection.execute("PRAGMA foreign_keys = ON;")
-        # auto vacuum to reclaim disk space
-        raw_connection.execute("PRAGMA auto_vacuum = FULL;")
 
         return raw_connection
 
