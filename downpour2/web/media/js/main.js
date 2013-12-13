36b196aa12c0e44a178b19a230276357ec40a54c
jjongsma <jeremy@jongsma.org>
2013-12-13 00:34:07 -0600

    Added responsive base template for desktop/mobile

---
+++ downpour2/web/media/js/main.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	$('#navigation, #header').click(function(e) {
+		e.stopPropagation();
+	});
+
+	$('#menu').click(function() {
+		if ($('#navigation').is(':visible')) {
+			$('#navigation').slideUp(100);
+		} else {
+			$('#navigation').slideDown(100);
+			$('html').click(function(e) {
+				$('#navigation').slideUp(100);
+			});
+		}
+	});
+
+});
+
+function spin(target) {
+	return new Spinner({
+		lines: 13, // The number of lines to draw
+		length: 20, // The length of each line
+		width: 10, // The line thickness
+		radius: 30, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 0, // The rotation offset
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		color: '#000', // #rgb or #rrggbb or array of colors
+		speed: 1, // Rounds per second
+		trail: 60, // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: 'auto', // Top position relative to parent in px
+		left: 'auto' // Left position relative to parent in px
+	}).spin(target);
+}
